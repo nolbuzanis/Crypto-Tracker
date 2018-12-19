08b041266efa18231a681c5cab635ed16f29fab8
nolbuzanis <nolanbuzanis@gmail.com>
2018-12-19 13:13:58 -0500

    grab data about bitcoin, ripple, ethereum

--- crypto_tracker.py
+++ crypto_tracker.py
@@ -2,4 +2,8 @@ import requests
 bitcoin_api_url = 'https://api.coinmarketcap.com/v1/ticker/'
 response = requests.request('GET', bitcoin_api_url)
 data = response.json()
-print(data[0:2])
+data_bitcoin = data[0]
+data_ripple = data[1]
+data_ethereum = data[2]
+
+print(data_ethereum)
