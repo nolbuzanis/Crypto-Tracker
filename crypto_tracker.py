f952222cf5bbbe5f649d51e83f61ee4dd3dae7f1
nolbuzanis <nolanbuzanis@gmail.com>
2018-12-19 13:07:07 -0500

    Print crypto data from coinmarketcap api

--- crypto_tracker.py
+++ crypto_tracker.py
@@ -1,4 +1,5 @@
 import requests
-bitcoin_api_url = 'https://api.coinmarketcap.com/v1/ticker/bitcoin/'
-request = requests.request('GET', bitcoin_api_url)
-print(request)
+bitcoin_api_url = 'https://api.coinmarketcap.com/v1/ticker/'
+response = requests.request('GET', bitcoin_api_url)
+data = response.json()
+print(data[0:2])
